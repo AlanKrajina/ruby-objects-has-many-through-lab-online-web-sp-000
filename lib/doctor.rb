f1b632f0103542c8171af295d1d4badabfc3ca53
Alan Krajina <alankrajina88@gmail.com>
2019-05-31 09:10:19 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -13,7 +13,7 @@ class Doctor
   end
   
   def new_appointment(date,patient)
-    appointment = Appointment.new(date,patient)
+    appointment = Appointment.new(date,self,patient)
     @appointments << appointment
     appointment.doctor = self
     appointment
